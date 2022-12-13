4d6557092035af4b7f0025ce4e87ad35ba6fa224
Sparrowhawk <jamiewilliamson@protonmail.com>
2022-12-13 12:58:27 -0500

    Creating the SpaceShip object that will be used in SpaceMain.

---
+++ src/game/SpaceShip.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.awt.Rectangle;
+import java.awt.Color;
+
+public class SpaceShip extends Rectangle {
+    //Notes: x,y,width,height (all ints) come from Rectangle
+    double xx, yy; //Double version of x,y for precise moving
+    double vx = 3.5;
+    double vy = 3.5;
+    Color clr = Color.GREEN; //TODO: make the enemy one a different colour
+
+    SpaceShip() {
+        xx = 200.0;
+        yy = 700.0;
+
+        x = (int) xx;
+        y = (int) yy;
+        width = 50;
+        height = 30;
+    }
+}
