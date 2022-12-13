0f5ae10820b2e57c809998252d80da567d94263d
Sparrowhawk <jamiewilliamson@protonmail.com>
2022-12-13 12:57:37 -0500

    Set up file with JFrame and Drawing Panel

--- src/game/SpaceMain.java
+++ src/game/SpaceMain.java
@@ -1,8 +1,35 @@
 package game;
 
-public class SpaceMain {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Dimension;
 
+public class SpaceMain {
     public static void main(String[] args) {
-        System.out.println("This will be the main game");
+        //TODO: add in the swingUtilities way of starting graphics
+        new SpaceMain();
+    }
+
+    int panW = 900;
+    int panH = 900;
+
+    DrawingPanel panel;
+    SpaceMain() {
+        panel = new DrawingPanel();
+        JFrame window = new JFrame("Salamder 2");
+        window.add(panel);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        //timer.start();
+    }
+
+    class DrawingPanel extends JPanel {
+        DrawingPanel() {
+            this.setBackground(Color.BLACK);
+            this.setPreferredSize(new Dimension(panW, panH));
+        }
     }
 }
