5c04ad6551cbe9b1f6e94d8fd95d1e92109eeefb
Sparrowhawk <jamiewilliamson@protonmail.com>
2022-12-20 12:07:27 -0500

    Testing

--- src/game/Laser.java
+++ src/game/Laser.java
@@ -1,10 +1,15 @@
 package game;
 
-import java.awt.Rectangle;
-
+import java.awt.*;
+/* This will just be moving vertically (initially)
+ */
 public class Laser extends Rectangle {
 
+    Color clr = Color.YELLOW;
+    int range = 1500; //how many pixels the laser will go
     Laser(SpaceShip player) {
+        width = 2;
+        height = 10;
 
     }
 }
